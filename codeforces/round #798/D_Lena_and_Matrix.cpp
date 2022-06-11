6f378611cc388a95b051a2bb53d8f70133495174
Vikash Yadav <incrediblevry0611@gmail.com>
2022-06-12 00:07:01 +0530

    upsolved cf round #798

--- codeforces/round #798/D_Lena_and_Matrix.cpp
+++ codeforces/round #798/D_Lena_and_Matrix.cpp
@@ -21,40 +21,33 @@ int main()
         pair<int,int> y={INT_MAX,INT_MIN};
 
         vector<string> mat(n);
+        pair<int,int> e1={n-1,m-1},e2={n-1,0},e3={0,m-1},e4={0,0};
+
         for (int i=0; i<n; i++){
             cin >> mat[i];
             for (int j=0; j<mat[i].size(); j++){
                 if (mat[i][j]=='B'){
-                    x.first = min(x.first,j);
-                    x.second = max(x.second,j);
-                    y.first = min(y.first,i);
-                    y.second = max(y.second,i);
+                    if (i+j < e1.first+e1.second) e1={i,j};
+                    if (i-j < e2.first-e2.second) e2={i,j};
+                    if (i-j > e3.first-e3.second) e3={i,j};
+                    if (i+j > e4.first+e4.second) e4={i,j};
                 }
             }
         }
-
-        int a1 = floor((double)(y.first + y.second)/2);
-        int a2 = ceil((double)(y.first + y.second)/2);
-        int b1 = floor((double)(x.first + x.second)/2);
-        int b2 = ceil((double)(x.first + x.second)/2);
-
-        vector<pair<int,int>> options = {{a1,b1},{a1,b2},{a2,b1},{a2,b2}};
-        vector<pair<int,int>> extremes;
-        if (mat[y.first][x.first]=='B') extremes.push_back({y.first,x.first});
-        if (mat[y.first][x.second]=='B') extremes.push_back({y.first,x.second});
-        if (mat[y.second][x.first]=='B') extremes.push_back({y.second,x.first});
-        if (mat[y.second][x.second]=='B') extremes.push_back({y.second,x.second});
+        vector<pair<int,int>> extremes = {e1,e2,e3,e4};
 
         pair<int,int> ans;
         int minmaxman = INT_MAX;
-        for (pair<int,int> i:options){
-            int maxman = INT_MIN;
-            for (pair<int,int> j:extremes){
-                maxman = max(maxman, man(i.first,i.second,j.first,j.second));
-            }
-x            if (maxman < minmaxman){
-                minmaxman = maxman;
-                ans = i;
+        for (int i=0; i<n; i++){
+            for (int j=0; j<m; j++){
+                int maxman = INT_MIN;
+                for (pair<int,int> e:extremes){
+                    maxman = max(maxman, man(i,j,e.first,e.second));
+                }
+                if (maxman < minmaxman){
+                    minmaxman = maxman;
+                    ans = {i,j};
+                }
             }
         }
 
